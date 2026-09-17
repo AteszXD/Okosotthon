ffeeca859d68d561c78ae0fb9c374341cf297a76
AteszXD <keria@kkszki.hu>
2026-09-17 10:19:03 +0200

    Add project files.

---
+++ Okosotthon/Program.cs
@@ -0,0 +1,16 @@
+﻿// 1. Okosotthon
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Okosotthon
+{
+    internal class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
